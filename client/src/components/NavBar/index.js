aea670519cdf21efe40031026a7cbb3088295ee1
K-ai30 <artpirate11@aim.com>
2020-05-04 19:23:43 -0400

    fixed navbar css

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -5,6 +5,7 @@ function NavBar() {
     return (
         <nav className="navbar navbar-light bg-light">
             <div>
+                <h3 id="appName">Google Books</h3>
                 <button id="searchBtn" className="navbar-brand mb-0 h1">Search</button>
                 <button id="savedBtn" className="navbar-brand mb-0 h1">Saved</button>
             </div>        
