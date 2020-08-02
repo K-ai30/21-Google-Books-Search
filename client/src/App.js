c549e4cee580795a608ab2c8e8d2fddca4232a25
K-ai30 <artpirate11@aim.com>
2020-08-02 16:32:35 -0400

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,65 @@
-import React from 'react';
-import API from './utils/API';
-import './assets/css/global.css';
+import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import API from "./utils/API";
+import "./assets/css/global.css";
 
-import NavBar from './components/NavBar';
-import Jumbotron from './components/Jumbotron';
-import Search from './components/BookSearch';
-import Results from './components/Results';
-import SavedBooks from './components/SavedBooks';
+import NavBar from "./components/NavBar";
+import Jumbotron from "./components/Jumbotron";
+import Search from "./components/BookSearch";
+import Results from "./components/Results";
+import SavedBooks from "./components/SavedBooks";
 
 export default class App extends React.Component {
+  // Create state
+  // state = {
+  //   bookList: [],
+  //   searchTerm: "",
+  //   // results: [],
+  //   alphaNumericOrder: true,
+  // };
 
-  state = { 
-    bookList: [],
-    searchTerm: ""
-  }
+  // componentDidMount() {
+  //   this.getBooks();
+  // }
 
-  componentDidMount() {
-    this.getBooks();
-  }
+  // getBooks() {
+  //   API.getBooks()
+  //     .then((res) => {
+  //       this.setState({ bookList: res.data.results });
+  //     })
+  //     .catch((err) => console.log(err));
+  // }
 
-  getBooks() {
-    API.getBooks().then( res => {
-      this.setState({ bookList: res.data.results });
-    })
-    .catch(err => console.log(err))
-  }
+  // handleClickEvent = (event) => {
+  //   const value = event.target.value;
+  //   if (!value) {
+  //     this.setState({ searchTerm: "" });
+  //     this.getBooks();
+  //   } else {
+  //     this.setState({
+  //       bookList: this.state.bookList.filter((book) => {
+  //         if (
+  //           employee.name.first.includes(value) ||
+  //           employee.name.last.includes(value) ||
+  //           employee.email.includes(value)
+  //         ) {
+  //           return employee;
+  //         }
+  //       }),
+  //       searchTerm: value,
+  //     });
+  //   }
+  // };
 
-  render () {
+  render() {
     return (
       <div className="App">
-        <NavBar/>
-        <Jumbotron/>
-        <Search/>
-        <Results/>
-        <SavedBooks/>
-    </div>
+        <NavBar />
+        <Jumbotron />
+        <Search />
+        <Results />
+        <SavedBooks />
+      </div>
     );
   }
 }
--- client/src/components/BookSearch/index.js
+++ client/src/components/BookSearch/index.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
 function BookSearch() {
-    return (
-        <div className="container">
-            <div className="card bg-light">
-            <div className="card-header bg-secondary text-white">
-            <i className="fas fa-book"></i> Book Search
-            </div>
-            <div className="card-body">
-                <h5 id="bookSearch" className="card-title">Book</h5>
-                <input id="field" type="book" className="form-control" id="exampleFormControlInput1" placeholder="Timeline"/>
-                <a id="search" href="#" className="btn btn-danger">Search</a>
-            </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="card bg-light">
+        <div className="card-header bg-secondary text-white">
+          <i className="fas fa-book"></i> Book Search
         </div>
-    )
+        <div className="card-body">
+          <h5 id="bookSearch" className="card-title">
+            Book
+          </h5>
+          <input
+            // value={this.state.value}
+            // onChange={(e) => {
+            //   this.handleChange(e);
+            // }}
+            id="field"
+            type="book"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="Timeline"
+          />
+          <a
+            // onClick={this.handleClick.bind(this)}
+            id="search"
+            href="#"
+            className="btn btn-danger"
+          >
+            Search
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default BookSearch;
