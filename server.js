2ef0b505c1e1ca7d553742d0d280650bb09da37f
K-ai30 <artpirate11@aim.com>
2020-08-02 18:13:13 -0400

    added deployment info

--- server.js
+++ server.js
@@ -1,28 +1,34 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
-const routes = require("./routes");
 const app = express();
-require("dotenv").config();
+const path = require('path');
+// require("dotenv").config();
 const PORT = process.env.PORT || 3012;
 
+const routes = require("./routes");
 const db = require("./models");
-
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb://<dbuser>:<dbpassword>@ds333248.mlab.com:33248/heroku_rdl96v68"
+    "mongodb://<dbuser>:<dbpassword>@ds333248.mlab.com:33248/heroku_rdl96v68",
+  { useNewUrlParser: true }
 );
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Add routes, both API and view
+app.use(routes);
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get('*', (req, res) => {
+    res.sendFile()path.join(_direname, 'client', 'build', 'index.hmtl');
+  });
+}
 
 // Start the API server
 app.listen(PORT, function () {
